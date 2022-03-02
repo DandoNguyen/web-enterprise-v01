0f73a08530c94a02e8269e270750de3400f243dc
NamHPGCS18027 <namhpgcs18027@fpt.edu.vn>
2022-03-02 21:14:54 +0700

    added the frontend app

---
+++ frontend_reactjs/src/App.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+import './App.css';
+import Homepage from './components/Homepage';
+import UserLogin from './components/UserLogin';
+
+
+function App() {
+  return <div>
+    <UserLogin/>
+  </div>
+
+}
+
+export default App;
