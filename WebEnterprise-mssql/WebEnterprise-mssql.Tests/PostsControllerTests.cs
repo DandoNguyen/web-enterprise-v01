9d85d1db5e3b4241d60ec186e3ba653c13f216b5
DandoNG <quangdungnguyen147@gmail.com>
2022-03-30 00:21:33 +0700

    move api.csproj to new folder / add testst.porject

---
+++ WebEnterprise-mssql/WebEnterprise-mssql.Tests/PostsControllerTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace WebEnterprise_mssql.Tests
+{
+    public class PostsControllerTests
+    {
+        [Fact]
+        public void GetAllPostsAsync_WithNoParam_ReturnListOfPosts()
+        {
+            //Arrange
+            
+            //Act
+
+            //Assert
+        }
+    }
+}
