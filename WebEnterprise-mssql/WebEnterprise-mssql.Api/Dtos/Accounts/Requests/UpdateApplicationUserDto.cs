7fd9ceaada181a233f0bace787c7cf88be4095ea
DandoNG <quangdungnguyen147@gmail.com>
2022-04-24 03:35:14 +0700

    Update user edit method

--- WebEnterprise-mssql/WebEnterprise-mssql.Api/Dtos/Accounts/Requests/UpdateApplicationUserDto.cs
+++ WebEnterprise-mssql/WebEnterprise-mssql.Api/Dtos/Accounts/Requests/UpdateApplicationUserDto.cs
@@ -7,12 +7,9 @@ namespace WebEnterprise_mssql.Api.Dtos
     {
         [Required]
         public string Email { get; set; }
-        public string userId { get; set; }
         public string UserName { get; set; }
-        public int? Age { get; set; }
         public string Address { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string fullname { get; set; }
         public DateTime? DOB { get; set; }
 
     }
